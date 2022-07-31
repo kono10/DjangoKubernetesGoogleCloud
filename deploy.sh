ea503312a5f285b59a01467a566cb93e138ba8b1
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-07-31 17:43:32 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t novskytech/djangoproject ./myproject --no-cache
+docker build -t novskytech/djangoproject:k8s ./myproject --no-cache
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 docker push novskytech/djangoproject:k8s
 kubectl apply -f k8s
